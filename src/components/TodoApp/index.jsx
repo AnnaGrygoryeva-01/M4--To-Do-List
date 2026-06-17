4c73ffde16611ce205827eb20f7ddd42c6fe2a1c
Anna Grygoryeva <annagrygoryeva3101@gmail.com>
2026-06-17 16:50:33 +0100

    Add functions to parent component

--- src/components/TodoApp/index.jsx
+++ src/components/TodoApp/index.jsx
@@ -1,13 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import TodoForm from "././TodoForm";
 import TodoList from "././TodoList";
 
 function TodoApp() {
+  const [tasks, setTasks] = useState([]);
+
+  const addTask = () => {
+    const newTask = {
+      id: Date.now(),
+      text: text,
+      isDone: false,
+    };
+    setTasks([...tasks, newTask]);
+  };
+
+  const deleteTask = (idToDelete) => {
+    const updatedList = tasks.filter((task) => task.id !== idToDelete);
+    setTasks(updatedList);
+  };
+
+  const toggleTask = (idToToggle) => {
+    const updatedTasks = tasks.map((task) => {
+      if (task.id === idToToggle) {
+        return { ...task, isDone: !task.isDone };
+      }
+      return task;
+    });
+    setTasks(updatedTasks);
+  };
+
   return (
-    <>
-      <TodoForm />
-      <TodoList />
-    </>
+    <article>
+      <h1>My To-Do List</h1>
+      <TodoForm addTask={addTask} />
+      <TodoList tasks={tasks} deleteTask={deleteTask} toggleTask={toggleTask} />
+    </article>
   );
 }
 export default TodoApp;
