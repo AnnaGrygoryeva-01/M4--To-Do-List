b70ff285426ef25fe3bd18603840dc68361c7a4e
Anna Grygoryeva <annagrygoryeva3101@gmail.com>
2026-06-22 17:57:47 +0100

    Add validation

--- src/components/TodoApp/TodoForm/index.jsx
+++ src/components/TodoApp/TodoForm/index.jsx
@@ -1,32 +1,57 @@
 import { useState } from "react";
 import styles from "./TodoForm.module.sass";
+import { object, string } from "yup";
+import cx from "classnames";
+
+const taskSchema = object({
+  taskText: string()
+    .trim()
+    .required("Field can't be empty")
+    .min(2, "Min 2 symbols are required")
+    .max(100, "Max 100 symbols"),
+});
 
 function TodoForm({ addTask }) {
   const [textInput, setTextInput] = useState("");
+  const [error, setError] = useState(null);
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    if (textInput.trim() === "") {
-      return;
+
+    try {
+      await taskSchema.validate({ taskText: textInput });
+
+      addTask(textInput);
+      setTextInput("");
+      setError(null);
+    } catch (err) {
+      setError(err.message);
     }
-    addTask(textInput);
-    setTextInput("");
   };
 
   const handleChange = (e) => {
     setTextInput(e.target.value);
+    if (error) setError(null);
   };
+
+  const inputClassName = cx(styles.inputField, {
+    [styles.inputError]: error,
+  });
 
   return (
     <form className={styles.formContainer} onSubmit={handleSubmit}>
-      <input
-        className={styles.inputField}
-        type="text"
-        value={textInput}
-        onChange={handleChange}
-        placeholder="Add your task..."
-      />
-      <button type="submit">+</button>
+      <div className={styles.inputWrapper}>
+        <input
+          className={inputClassName}
+          type="text"
+          value={textInput}
+          onChange={handleChange}
+          placeholder="Add your task..."
+        />
+        <button type="submit">+</button>
+      </div>
+
+      {error && <p className={styles.errorText}>{error}</p>}
     </form>
   );
 }
